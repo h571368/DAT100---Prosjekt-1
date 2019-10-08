ab944c8bd21851fb2cd4d87be1cda9808933da95
h571368 <54029563+h571368@users.noreply.github.com>
2019-10-08 14:17:29 +0200

    lagt til int test = 0 i TODO!

--- src/no/hvl/dat100ptc/TODO.java
+++ src/no/hvl/dat100ptc/TODO.java
@@ -3,12 +3,12 @@ package no.hvl.dat100ptc;
 public class TODO {
 
 	public static String method() {
-		
+		int test = 0;
 		String methodName = Thread.currentThread().getStackTrace()[2].getMethodName();
 		
 		return "Metoden " + methodName + " er ikke implementert";
 	}
-	
+	//TEST1
 	
 	public static String construtor(String className) {
 				
